398a86de5597bfd811eb75ae7248c488bdb1a0a4
MatthewNicastro <15mrn1@queensu.ca>
2021-06-05 16:37:58 -0400

    more flask app configuration

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def root():
-    return app.send_static_file('index.html')
+    return app.send_static_file('html/index.html')
 
 if __name__ == "__main__": 
     app.run()
